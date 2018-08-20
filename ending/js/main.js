32018bcf4ea12570024799a18839ae6a0320a162
RomaNovo <roma_1985_roma@mail.ru>
2018-08-20 22:29:51 +0300

    added  function for input

--- ending/js/main.js
+++ ending/js/main.js
@@ -1,23 +1,40 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-	let form = document.querySelector('form');
-	let sbmt = document.querySelector('.form__submit');
-	let btn = document.querySelector('.section-form__button');
-	let message = document.querySelector('.section-form__message');
-	let inpts = document.querySelectorAll('input');
+	let form 	= document.querySelector('form'),
+		sbmt 	= document.querySelector('.form__submit'),
+		btn 	= document.querySelector('.section-form__button'),
+		message = document.querySelector('.section-form__message'),
+		inpts 	= document.querySelectorAll('[data-type]');
 
 	let types = {
 	  'email': /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
 	  'text': /.+/,
 	}
 
+	let inputs = [...form.querySelectorAll('[data-type]')];
+	inputs.forEach( (item) => {
+		let input = item;
+		
+		input.addEventListener('input', () => {
+
+			( types[input.dataset.type].test(input.value) ) ? 
+				input.classList.remove('form__input_novalid') :
+				input.classList.add('form__input_novalid');
+			
+			if( input.getAttribute('type') === 'password' ){
+				( input.value.length < 6 ) ? 
+					input.classList.add('form__input_novalid') :	
+					input.classList.remove('form__input_novalid');			
+			};
+		})
+	})
+
 	let validate = (form) => {
-	  let inputs = [...form.querySelectorAll('[data-type]')];
-	  let passed = true;
-	  let password;
-	  
+	  let inputs = [...form.querySelectorAll('[data-type]')],
+	  	  passed = true;
+	   
 	  inputs.forEach((item) => {
 	  
-	    if ((types[item.dataset.type] && types[item.dataset.type].test(item.value)) || (item.value == password && item.value != '')){
+	    if ( (types[item.dataset.type] && types[item.dataset.type].test(item.value)) ){
 	      item.classList.remove('form__input_novalid');
 	    }
 	    else{
@@ -26,28 +43,20 @@ document.addEventListener('DOMContentLoaded', ()=> {
 	    }
 	  });
 
-	  if( (inputs[2].value.length < 6) || (inputs[2].value !== inputs[3].value)) {
-	  	console.log(inputs[2].value.length);
+	  if( (inputs[2].value.length < 6) || (inputs[2].value !== inputs[3].value) ) {
 	  	inputs[2].classList.add('form__input_novalid');
 	  	inputs[3].classList.add('form__input_novalid');
 	  	passed = false;
 	  }
 	  
-	  return passed;
-	/*  if(passed) {
-	  	let message `"Thank you for registering the ${inputs[].value.toUpperCase} from the city ${inputs[].value.toUpperCase()}."`;
-	  }*/
-	  
+	  return passed;  
 	};
 
 	sbmt.addEventListener('click', (e)=> {
 		e.preventDefault();
 		if(validate(form)) {
 			let arr = form.querySelectorAll('[data-id]');
-			console.log(arr);
-			
-			message.innerHTML = `"Thank you for registering the ${arr[0].value.toUpperCase()} from the city ${arr[1].value.toUpperCase() || 'KIEV'}."`;
-
+			message.innerHTML = `"Thank you for registering the ${arr[0].value.toUpperCase()} from the city ${arr[1].value.toUpperCase()|| ['KIEV', 'ODESSA', 'LVIV', 'DONETSK', 'KHARKIV'][Math.floor(Math.random() * 5)]}."`;
 			form.style.display = 'none';
 			btn.style.display = 'block';
 			message.style.display = 'block';
